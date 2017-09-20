7e16a1d883de815a20e6cfca53623a79076af7f7
vagrant <vagrant@homestead>
2017-09-20 08:57:50 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/pathshow','StaticPageController@pathshow');
 Route::get('/wikishow','StaticPageController@wikishow');
 Route::get('signup','UsersController@create')->name('signup');
 Route::resource('users','UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
